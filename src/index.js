c49bf630fc4c7432fcb93d8894964e7af3787e8a
Jakub <jakub.szaredko@gmail.com>
2021-09-21 21:41:28 +0200

    Create basic input handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './scss/index.scss';
+
+const calculator = document.querySelector('.agh-calculator__form');
+
+calculator.addEventListener('input', e => {
+    const t = e.target;
+    if (!t.classList.contains('agh-calculator__slider')) return;
+
+    t.nextElementSibling.value = t.value;
+});
