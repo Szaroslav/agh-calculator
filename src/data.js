59edf1a263b3d3b86b8f6d64c10e440f0cd7f36e
Jakub <jakub.szaredko@gmail.com>
2021-09-22 14:09:58 +0200

    Create elementary fields of study data

---
+++ src/data.js
@@ -0,0 +1,54 @@
+const data = {
+    faculties: [
+        {
+            name: 'Wydział Inżynierii Lądowej i Gospodarki Zasobami',
+            fields: [
+                {
+                    name: 'Budownictwo',
+                    subjects: [
+                        [ 'maths', 'physics', 'chemistry', 'computer science' ],
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'foreign language' ]
+                    ]
+                },
+                {
+                    name: 'Inżynieria Górnicza',
+                    subjects: [
+                        [ 'maths', 'physics', 'chemistry', 'computer science' ],
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'geography', 'foreign language' ]
+                    ]
+                },
+                {
+                    name: 'Inżynieria i Zarządzanie Procesami Przemysłowymi',
+                    subjects: [
+                        [ 'maths', 'physics', 'chemistry', 'computer science' ],
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'foreign language' ]
+                    ]
+                },
+                {
+                    name: 'Inżynieria Kształtowania Środowiska',
+                    subjects: [
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'biology' ],
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'biology', 'foreign language', 'geography' ]
+                    ]
+                },
+                {
+                    name: 'Rewitalizacja Terenów Zdegradowanych',
+                    subjects: [
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'biology' ],
+                        [ 'maths', 'physics', 'chemistry', 'computer science', 'biology', 'foreign language', 'geography' ]
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Wydział Inżynierii Metali i Informatyki Przemysłowej',
+            fields: [
+                '',
+                '',
+                '',
+                '',
+                ''
+            ]
+        },
+    ]
+};
